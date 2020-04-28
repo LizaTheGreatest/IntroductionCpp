e4c2d4ba78b58416dc97c6da29f227c697ef5f45
Liza Phan <phantr98@gmail.com>
2020-04-28 19:02:11 +0300

    arrays.h used

--- Arrays and pointers/13.cpp
+++ Arrays and pointers/13.cpp
@@ -1,72 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "arrays.h"
 
 
 //--------------------------------#13---------------------------------
-void addMatrix(int***, int, int);
-void fillMatrix(int***, int, int);
-void viewMatrix(int**, int, int);
-void findMinMaxSum(int**, int, int*, int*, int*);
+
+void findMinMaxSum(int** matrix, int rows, int columns, int* min, int* max, int* sum);
 int main()
 {
     int** matrix;
-    int n;
+    int rows, columns;
     int min, max, sum;
 
-    printf("Enter matrix size: ");
-    scanf("%d", &n);
+    printf("Enter matrix number of rows: ");
+    rows = safeReadInt();
 
-    addMatrix(&matrix, n, n);
-    fillMatrix(&matrix, n, n);
-    viewMatrix(matrix, n, n);
+    printf("Enter matrix number of columns: ");
+    columns = safeReadInt();
 
-    findMinMaxSum(matrix, n, &min, &max, &sum);
+    createMatrix(&matrix, rows, columns);
+    fillMatrix(matrix, rows, columns);
+
+    viewMatrix(matrix, rows, columns);
+
+
+    findMinMaxSum(matrix, rows, columns, &min, &max, &sum);
 
     printf("\nSum is %d\nMinimum is %d\nMaximum is %d\n", sum, min, max);
 
     return 0;
 }
 
-void addMatrix(int*** matrix, int n, int m)
-{
 
-    *matrix = (int**) malloc(n * sizeof (int*));
-    for(int i = 0; i < n; ++i)
-        (*matrix)[i] = (int*) malloc(m * sizeof (int));
-
-}
-
-
-void fillMatrix(int*** matrix, int n, int m)
-{
-    for(int i = 0; i < n; ++i)
-    {
-        printf("Enter %d elements: ", m);
-        for(int j = 0; j < m; ++j)
-            scanf("%d", &(*matrix)[i][j]);
-    }
-}
-
-
-void viewMatrix(int** matrix, int n, int m)
- {
-    printf("\n\tMATRIX %d x %d\n\n", n, m);
-     for(int i = 0; i < n; ++i)
-     {
-         for(int j = 0; j < m; ++j)
-             printf("%d\t", matrix[i][j]);
-         printf("\n\n");
-     }
- }
-
-
-void findMinMaxSum(int** matrix, int n, int* min, int* max, int* sum)
+void findMinMaxSum(int** matrix, int rows, int columns, int* min, int* max, int* sum)
 {
     *min = matrix[1][0];
     *max = matrix[0][1];
     *sum = 0;
-    for(int i = 0; i < n; ++i)
-        for(int j = 0; j < n; ++j)
+    for(int i = 0; i < rows; ++i)
+        for(int j = 0; j < columns; ++j)
         {
             if(j < i)
             {
