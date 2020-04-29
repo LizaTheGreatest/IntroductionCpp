5ad7b9858f4e986fec235b0bf616eb326275cc22
Liza Phan <phantr98@gmail.com>
2020-04-29 17:07:25 +0300

    arrays.h added

--- Arrays and pointers/17.cpp
+++ Arrays and pointers/17.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "arrays.h"
 
 //--------------------------------#17---------------------------------
 void getFirstIndex(int**, int*, int*, int, int);
@@ -8,81 +8,108 @@ void getLastIndex(int**, int*, int*, int, int);
 
 int main()
 {
-    int n, m;
-    int i1, j1, i2, j2;
+    int rows, columns;
+    int** matrix;
+
+    int x1, y1, x2, y2;
     int multipl = 1;
 
-    printf("Enter number of rows and columns: ");
-    scanf("%d%d", &n, &m);
 
-    int** matrix = (int**) malloc(n * sizeof (int*));
-    for(int i = 0; i < n; ++i)
-        matrix[i] = (int*) malloc(m * sizeof (int));
+    printf("Enter number of rows: ");
+    rows = safeReadInt();
 
-    for(int i = 0; i < n; ++i)
+    printf("Enter number of columns: ");
+    columns = safeReadInt();
+
+    createMatrix(&matrix, rows, columns);
+    fillMatrix(matrix, rows, columns);
+    viewMatrix(matrix, rows, columns);
+
+
+    getFirstIndex(matrix, &x1, &y1, rows, columns);
+    getLastIndex(matrix, &x2, &y2, rows, columns);
+
+    printf("%d %d\n", x1, y1);
+    printf("%d %d\n", x2, y2);
+
+    int first_i, last_i;
+    int first_j;
+
+    if(y1 == columns - 1)
+    {
+        first_i = x1 + 1;
+        first_j = 0;
+    }
+    else
+    {
+        first_i = x1;
+        first_j = y1 + 1;
+    }
+
+    if(y2 == 0)
+    {
+        last_i = x2 - 1;
+
+    }
+
+    else
+    {
+        last_i = x2;
+
+    }
+
+    bool t = true;
+    for(int i = first_i; i <= last_i; ++i)
+    {
+        if(!t) break;
+        for(int j = first_j; j < columns; ++j)
         {
-            printf("Enter %d elements: ", m);
-            for(int j = 0; j < m; ++j)
-                scanf("%d", &matrix[i][j]);
+            if(j == y2 && i == x2)
+            {
+                t = false;
+                break;
 
+            }
+
+            if(j == columns - 1)
+                first_j = 0;
+
+            multipl *= matrix[i][j];
         }
 
-    for(int i = 0; i < n; ++i)
-    {
-        for(int j = 0; j < m; ++j)
-            printf("%d\t", matrix[i][j]);
-        printf("\n\n");
     }
 
-    getFirstIndex(matrix, &i1, &j1, n, m);
-    getLastIndex(matrix, &i2, &j2, n, m);
-
-    printf("%d %d\n", i1, j1);
-    printf("%d %d\n", i2, j2);
-    for(int i = i1; i < i2; ++i)
-    {
-        int j;
-        if(i == i1)
-            j = j1 + 1;
-        else j = 0;
-        int last;
-        if(i == i2)
-            last = j2;
-        else last = m;
-        for(; j < last; ++j)
-            multipl *= matrix[i][j];
-
-    }
     printf("%d\n", multipl);
 
 
     return 0;
 }
 
-void getFirstIndex(int** matrix, int* i1, int* j1, int n, int m)
+void getFirstIndex(int** matrix, int* x1, int* y1, int rows, int columns)
 {
-    for(int i = 0; i < n; ++i)
-        for(int j = 0; j < m; ++j)
+    for(int i = 0; i < rows; ++i)
+        for(int j = 0; j < columns; ++j)
         {
             if(matrix[i][j] < 0)
             {
-                *i1 = i;
-                *j1 = j;
+                *x1 = i;
+                *y1 = j;
                 return;
             }
         }
 }
 
-void getLastIndex(int** matrix, int* i2, int* j2, int n, int m)
+void getLastIndex(int** matrix, int* x2, int* y2, int rows, int columns)
 {
-    for(int i = n - 1; i >= 0; --i)
-        for(int j = m - 1; j >= 0; --j)
+    for(int i = rows - 1; i >= 0; --i)
+        for(int j = columns - 1; j >= 0; --j)
         {
             if(matrix[i][j] < 0)
             {
-                *i2 = i;
-                *j2 = j;
+                *x2 = i;
+                *y2 = j;
                 return;
             }
         }
 }
+
